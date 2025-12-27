48c76afb74d46a32b5dec3e8c88f940a083035a3
Kuma <kumapauze@gmail.com>
2025-12-27 13:12:43 -0500

    Add Y-Sort

--- scripts/tree.gd
+++ scripts/tree.gd
@@ -1,7 +1,32 @@
+@tool
 extends Node
+
+@export_tool_button("Update") var update = update_visuals
+@export var is_shattered : bool
+@export var animator : AnimationPlayer
+
+func _enter_tree() -> void:
+	update_visuals()
 
 func on_area_entered(area: Area2D) -> void:
 	if !area.is_in_group("player"):
 		return
 	
+	if is_shattered:
+		return
+	
+	var current_mask : MaskResource = area.get_parent().get_held_mask()
+	if current_mask != null && current_mask.mask_type == MaskResource.MASK_TYPES.BEAR:
+		animator.play("shattered")
+		return
+	
 	area.get_parent().advance_time()
+
+func update_visuals() -> void:
+	if animator == null:
+		return
+	
+	if !is_shattered:
+		animator.play("RESET")
+	else:
+		animator.play("shattered")
